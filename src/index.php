630ae19cc5d5b3ff72dac11e574d046f4e409bca
alanx099 <alan.hodak@gmail.com>
2022-10-06 12:54:23 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,16 +1,10 @@
 <?php
 
 function pingAddress($ip) {
-    $pingresult = exec("/bin/ping -n 3 $ip", $outcome, $status);
-    if (0 == $status) {
-        $status = "alive";
-    } else {
-        $status = "dead";
-    }
-    echo "The IP address, $ip, is  ".$status;
+    exec("/bin/ping $ip", $outcome, $status);
+    echo json_encode($outcome);
 }
 
-pingAddress("www.google.hr");
 pingAddress("193.198.163.108");
 
 ?>
