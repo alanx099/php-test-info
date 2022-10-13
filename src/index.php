1214b7baf628eb12d7fa66ab357288597ad69355
alanx099 <alan.hodak@gmail.com>
2022-10-13 12:45:47 +0200

    afd

--- src/index.php
+++ src/index.php
@@ -1,12 +1,5 @@
 <?php
 
-function pingAddress($ip) {
-    $drejk = exec("/bin/ping $ip", $outcome, $status);
-    echo $drejk;
-    echo json_encode($outcome);
-    echo $status;
-}
-
-pingAddress("193.198.163.108");
+echo($_ENV['CONNECTION']);
 
 ?>
